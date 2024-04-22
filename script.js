ce9438b16a531b4972aff2d880c7cfde122d3bb2
Reagan Konkle <regosmallz@gmail.com>
2024-04-22 12:34:23 -0400

    Create function to get human choice of RPS

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function getComputerChoice() {
     else if (randomNum > 0.66)
         return "scissors";
 }
+
+function getHumanChoice() {
+    let choice = prompt("Choose your weapon: Rock, Paper, or Scissors?");
+
+    return choice.toLowerCase();
+}
+
