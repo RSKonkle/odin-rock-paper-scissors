3d0f73f10aef6aa04b92457b6702c0e5fc915316
Reagan Konkle <regosmallz@gmail.com>
2024-04-22 13:59:05 -0400

    Nest playRound function in new playGame function - calling playRound 5 times

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let randomNum = Math.random();
-    
+
     if (randomNum <= 0.33)
         return "rock";
     else if (randomNum > 0.33 && randomNum <= 0.66)
@@ -11,31 +11,41 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Choose your weapon: Rock, Paper, or Scissors?");
-
     return choice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return (`Tie! You both chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`);
-    }
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
 
-    else if (
-        (humanChoice === "rock" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "rock")
-        ){
-        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} \
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`Tie! You both chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`);
+        }
+    
+        else if (
+            (humanChoice === "rock" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "rock")
+            ){
+            console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} \
 beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`);
-        computerScore++;
+            computerScore++;
+        }
+    
+        else { 
+            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} \
+beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}!`);
+            humanScore++;
+        }
     }
 
-    else { 
-        console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} \
-beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}!`);
-        humanScore++;
-    }
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`Final Score: Player - ${humanScore}, Computer ${computerScore}`);
 }
 
-let computerScore = 0;
-let humanScore = 0;
+playGame()
